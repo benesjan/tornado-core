fd85b0465af094ac57c5975fbd3a4fa21bfd1ba8
benesjan <janbenes1234@gmail.com>
2020-05-18 14:19:47 +0200

    Redeployed tornados to avoid

--- config.js
+++ config.js
@@ -5,13 +5,9 @@ module.exports = {
     netId3: {
       pbtc: {
         instanceAddress: {
-          // 0.2 ETH was deployed for each instance into the RelayHub
-          0.001: '0xb5c512E013c1f17a5ed157c557c6891558f1a719',
-          0.01: '0x0bD3D556707bEFe41C6215Dc8bf06D81616D6112',
-          0.1: '0x731a0e7c35dC9b7002be432770F78aF99f2FAf02',
-          1: undefined,
-          10: undefined,
-          100: undefined
+          '0.001': '0x8425EBC05AC74338838A0D99Db495906dF2eAe22',
+          '0.01': '0x2f2d3E612F3341eCDA859f2eB51b3a51b8eB62BB',
+          '0.1': '0xE9CaA191fc0D5E0C7fEE83b39F008Ba89b75df13',
         },
         tokenAddress: '0xEB770B1883Dcce11781649E8c4F1ac5F4B40C978',
         symbol: 'pBTC',
